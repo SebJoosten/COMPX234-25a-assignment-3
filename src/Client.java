bf668f4d6d1ad1c3d00f86ce63a8b8da581a9a84
SebJoosten <sebjoosten@gmail.com>
2025-05-16 15:09:21 +1200

    regactor and slim - also retry loops in client cleaned up

--- src/Client.java
+++ src/Client.java
@@ -11,6 +11,7 @@ public class Client {
     }
 
     public void runClient() {
+        int retryCount = 0;
 
         // retry loop for initializing
         while(true) {
@@ -19,85 +20,94 @@ public class Client {
             String fileName = "temoprary";
             DatagramSocket receiver = null;
 
+            // TRY and make socket and parse response
             try (DatagramSocket socket = new DatagramSocket()) {
                 socket.setSoTimeout(10000);
-
-                // Initial request
                 InetAddress serverAddress = InetAddress.getByName("localhost");
+
+                // INITIAL request
                 String initialMessage = "DOWNLOAD " + fileName;
                 DatagramPacket sendPacket = new DatagramPacket(initialMessage.getBytes(), initialMessage.length(), serverAddress, port);
-                System.out.println("Initial Sending: " + initialMessage);
                 socket.send(sendPacket);
 
-                // Receive response and break apart for assessment
+                // RECEIVE response and SPLIT
                 buffer = new byte[2048];
                 DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length, serverAddress, port);
                 socket.receive(receivePacket);
                 String message = new String(receivePacket.getData(), 0, receivePacket.getLength()).trim();
-                System.out.println("Initial Received: " + message);
                 String[] split = message.split(" ");
 
-                // OK found > check port and size parse
+                // RESPONSE OK
                 if (split.length == 6 && split[0].equals("OK") && split[1].equals(fileName) ) {
-                    long size , current = 0;
-                    int newPort;
-                    try {
-                        size = Long.parseLong(split[3]);
-                        newPort = Integer.parseInt(split[5]);
 
-                        while(true){
+                    long current = 0, size = Long.parseLong(split[3]);
+                    int  newPort = Integer.parseInt(split[5]);
+                    System.out.println("File transfer "+ fileName + " started on port " + newPort);
+                    String requestPart = "FILE " + fileName + " GET START " + current + " END " + (current + 1000);
+                    System.out.println("CLIENT connected on port: "  + newPort);
+                    retryCount = 0;
 
-                            // Send request
-                            String requestPart = "FILE " + fileName + " GET START " + current + " END " + (current + 1000);
-                            sendPacket = new DatagramPacket(requestPart.getBytes(), requestPart.length(), serverAddress, newPort);
-                            System.out.println("Secondary Sending: " + requestPart);
-                            socket.send(sendPacket);
+                    // START DOWNLOAD LOOP - send data request
+                    while(retryCount < 10){
+                        sendPacket = new DatagramPacket(requestPart.getBytes(), requestPart.length(), serverAddress, newPort);
+                        socket.send(sendPacket);
 
-                            // receive request
-                            buffer = new byte[2048];
-                            receivePacket = new DatagramPacket(buffer, buffer.length, serverAddress, newPort);
-                            socket.receive(receivePacket);
-                            message = new String(receivePacket.getData(), 0, receivePacket.getLength()).trim();
-                            System.out.println("Received: " + message);
-                            split = message.split(" ");
+                        // RECEIVE data packet convert to string and split
+                        receivePacket = new DatagramPacket(buffer, buffer.length, serverAddress, newPort);
+                        socket.receive(receivePacket);
+                        message = new String(receivePacket.getData(), 0, receivePacket.getLength()).trim();
+                        split = message.split(" ");
 
-                            if (split.length == 9 && split[0].equals("FILE") && split[1].equals(fileName) && split[2].equals("OK") ) {
-                                String data = "";
-                                int start = 0, end = 0;
+                        // CHECK DATA
+                        if (split.length == 9 && split[0].equals("FILE") && split[1].equals(fileName) && split[2].equals("OK") ) {
+                            String data = "";
+                            int start = 0, end = 0;
+                            try {
+                                start = Integer.parseInt(split[4]);
+                                end = Integer.parseInt(split[6]);
+                                data = split[8];
+                                retryCount = 0;
 
-                                // Parse data
-                                try {
-                                    start = Integer.parseInt(split[4]);
-                                    end = Integer.parseInt(split[6]);
-                                    data = split[8];
-                                } catch (NumberFormatException e) {
-                                    throw new NumberFormatException("ERROR: Port or size number failed to parse");
-                                }
+                                // if ok request next part of file
+
+
+                            // CATCH RESPONSE data not parsable - print message - inc retryCount - retry
+                            } catch (NumberFormatException e) {
+                                System.out.println("ERROR: parsing INT GOT --> " + message + " ON PORT: " + newPort);
+                                retryCount++;
+                                continue;
                             }
 
-                            requestPart = "FILE " + fileName + " CLOSE";
-                            sendPacket = new DatagramPacket(requestPart.getBytes(), requestPart.length(), serverAddress, newPort);
-                            socket.send(sendPacket);
-                            socket.close();
-                            throw new IOException("Socket closed");
+                            // IF FILE COMPLETE --------------- NEEDS CLOSING CONDICTION
+                            if(current < size) requestPart = "FILE " + fileName + " CLOSE";
 
-                        }// end download loop
-                    } catch (NumberFormatException e) {
-                        throw new NumberFormatException("ERROR: Port or size number failed to parse");
-                    }
+                        // FILE CLOSE OK - Throw to exit
+                        } else if (split.length == 3 && split[0].equals("FILE") && split[1].equals(fileName) && split[2].equals("CLOSE_OK")) {
+                            retryCount = 10;
+                            throw new IOException("FILE CLOSE OK ");
+                        } else System.out.println("ERROR: parsing GOT -- > " + message + " ON PORT: " + newPort);
 
-                // Else error message
+                        retryCount ++;
+
+                    // RETRIES EXCEEDED - Throw to exit
+                    }  throw new IOException("ERROR: parsing Response after 10 retries");
+
+                // FILE NOT FOUND - Throw to exit
                 } else if (split.length == 2 && split[0].equals("ERR") && split[1].equals("NOT_FOUND") ) {
-                    System.out.println("File not found ... Exit");
                     socket.close();
-                    System.exit(0);
+                    retryCount = 10;
+                    throw new IOException("FILE NOT FOUND ");
                 }
 
+            // CATCH INITIAL parsing and IO errors - check for EXIT
             } catch (IOException e) {
-                System.out.println("ERROR: " + e.getMessage());
+                System.out.println(e.getMessage() + " " + retryCount + "/10" );
+                retryCount++;
+                if (retryCount >= 10) {
+                    System.out.println(e.getMessage() + " EXITING ......");
+                    System.exit(0);
+                }
             }
-            System.out.println("Retry Send.....");
-
-        }// main loop
-    }// Run end
+        }
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -11,68 +11,54 @@ public class Server {
     public void runServer(String[] args) {
         DatagramSocket socket = null;
         int port = 51234;
+        byte[] receiveData = new byte[2048];
 
-        while (true) {              // Main while loop keeps re trying
-            try {                   // Start listening and set timeout and variable
-                socket = new DatagramSocket(port);
-                socket.setSoTimeout(10000);
-                byte[] receiveData = new byte[2048];
+        while (true) {
+            try {   socket = new DatagramSocket(port);
+                    socket.setSoTimeout(10000);
 
-                while (true) {
+                    // Reserve request Split and parse
                     DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                     socket.receive(receivePacket);
-
-                    // Convert a message to string > parse and respond
                     String message = new String(receivePacket.getData(), 0, receivePacket.getLength());
                     String response = "ERR NOT_PARSEABLE";
-                    String[] parts;
-                    if (message.split(" ", 2).length == 2) {
-                        parts = message.split(" ", 2);
-                        String command = parts[0];
-                        String data = parts.length > 1 ? parts[1] : "";
+                    String[] parts = message.split(" ");
 
-                        // Check for command
-                        if (command.equals("DOWNLOAD")) {
-                            if (!data.isEmpty()) {
+                    // COMMAND OK
+                    if (parts.length == 2 && parts[0].equals("DOWNLOAD")) {
+                        String fileName = parts[1];
 
-                                //************ VVV REPLACE CODE VVV ************
-                                // Replace with file name check
-                                String fileName = "temoprary";
-                                System.out.println(data);
-                                boolean fileExists = true; // Will work this out later placeholder
-                                //************ ^^^ REPLACE CODE ^^^ ************
+                        // CHECK FOR FILE HERE
 
-                                if (fileExists) {
-                                    //************ VVV REPLACE CODE VVV ************
-                                    // Replace with file details
-                                    // Get file details
-                                    //File file = new File("example.txt");
-                                    //long fileSize = file.length();
-                                    long fileSize = 123456;
-                                    //************ ^^^ REPLACE CODE ^^^ ************
+                        // FILE OK
+                        if (!fileName.isEmpty()) {
 
-                                    int freePort = portFinderUDPPort();
-                                    if (freePort != -1) {
-                                        response = "OK " + fileName + " SIZE " + fileSize + " PORT " + freePort;
-                                        Thread thread = new Thread(new FileMoverTask(fileName, freePort));
-                                        thread.start();
-                                    } else response = "ERR NO_FREE_PORT";
-                                } else response = "ERR NOT_FOUND";
-                            } // Command wrong response = "ERR NOT_PARSEABLE"
-                        } // Command wrong response = "ERR NOT_PARSEABLE"
-                    } // Split wrong response = "ERR NOT_PARSEABLE"
+                            long fileSize = 123456;
+                            int freePort = portFinderUDPPort();
 
-                    // Prep and send a return packet
-                    System.out.println("Server: " + response + " : " + message);
+                            // PORT OK
+                            if (freePort != -1) {
+                                response = "OK " + fileName + " SIZE " + fileSize + " PORT " + freePort;
+                                Thread thread = new Thread(new FileMoverTask(fileName, freePort));
+                                thread.start();
+
+                            } else response = "ERR NO_FREE_PORT";
+                        } else response = "ERR NOT_FOUND";
+                    }
+
+                    // Tiny delay then send a packet this is so the thread has time to start
+                    Thread.sleep(5);
                     DatagramPacket sendPacket = new DatagramPacket(response.getBytes(), response.length(), receivePacket.getAddress(), receivePacket.getPort());
                     socket.send(sendPacket);
+                    socket.close();
 
-                } // Main loop end
             } catch (IOException e) {
                 System.out.println("ERROR: " + e.getMessage());
                 if (socket != null) socket.close();
-            } // Sub loop
-        } // main loop
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     /**
@@ -92,10 +78,10 @@ public class Server {
      * File mover thread class used for spinning off a sending thread
      */
     private class FileMoverTask implements Runnable {
-        private int port;
+        private int port , retryCount = 0;
         private String fileName , ID ;
         private byte[] receiveData = new byte[2048];
-        private DatagramSocket socket = null;
+        private DatagramSocket newSocket = null;
 
         /**
          * This is what you call to start a new thread running for the file transfur
@@ -110,23 +96,23 @@ public class Server {
 
         @Override
         public void run() { System.out.println("LOG: " + ID + " STARTED" );
-
             try  {
-                socket = new DatagramSocket(port);
-                socket.setSoTimeout(15000);
+                newSocket = new DatagramSocket(port);
+                newSocket.setSoTimeout(10000);
                 String message, response;
 
-                // Start listening loop. Resets when a packet is formatted incorrectly
+                // RECEIVE data request and REPLY
                 while (true) {
                     DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                    socket.receive(receivePacket);
+                    newSocket.receive(receivePacket);
                     message = new String(receivePacket.getData(), 0, receivePacket.getLength()).trim();
                     String[] split = message.split(" ");
 
-                    // Check the received packet > make response and send it otherwise restart
+                    // IF OK grab the data block
                     if (split.length == 7 && split[0].equals("FILE") && split[1].equals(fileName) && split[2].equals("GET") && split[3].equals("START") && split[5].equals("END")) {
                         int start = Integer.parseInt(split[4]);
                         int end = Integer.parseInt(split[6]);
+                        retryCount = 0;
 
 
                         //  get the file content and formulate response string else re try
@@ -134,20 +120,21 @@ public class Server {
 
 
 
-                    // Check for close
+                    // IF CLOSE OR NOT RECOGNISED
                     } else if(split.length == 3 && split[0].equals("FILE") && split[1].equals(fileName) && split[2].equals("CLOSE")){
                         response = "FILE " + fileName + " CLOSE_OK";
-                    } else continue;
+                    } else {
+                        response = "ERR INVALID_COMMAND";
+                        retryCount++;
+                    }
 
-                    // Send
+                    // SEND PACKET - CHECK EXIT
                     DatagramPacket sendPacket = new DatagramPacket(response.getBytes(), response.length(), receivePacket.getAddress(), receivePacket.getPort());
-                    socket.send(sendPacket);
-                    if (response.contains("CLOSE_OK")) break; // if close then exit the loop
+                    newSocket.send(sendPacket);
+                    if (response.contains("CLOSE_OK") || retryCount > 10) break;
+                }
 
-                } // Loop until the file is finished
-            } catch (IOException e) {
-                System.out.println("ERROR: " + ID + e.getMessage());
-            }
+            } catch (IOException e) { System.out.println("ERROR: " + ID + e.getMessage()); }
             System.out.println("LOG: " + ID + " CLOSED" );
         }
     }
